7a26ceb872584767c8f51151f1b6e94cc822505e
laxmankumar2000 <laxmankumarvashisth@gmail.com>
2022-04-25 09:46:18 +0530

    Maximum_sum_Bst_IN_BT

---
+++ src/Lec_26/Maximum_Sum_BST_in_BT.java
@@ -0,0 +1,60 @@
+package Lec_26;
+
+public class Maximum_Sum_BST_in_BT {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    public class BstPair {
+        boolean isbst = true;
+        long min = Long.MAX_VALUE;
+        long max = Long.MIN_VALUE;
+        int sum = 0;  // bt
+        int ans = 0; // bst
+
+    }
+
+    class Solution {
+        public int maxSumBST(TreeNode root) {
+            return BSTsum(root).ans;
+        }
+
+        public BstPair BSTsum(TreeNode root) {
+            if(root ==null)
+                return new BstPair();
+
+            BstPair lpbst = BSTsum(root.left);
+            BstPair rpbst = BSTsum(root.right);
+            BstPair spbst = new BstPair();
+
+            spbst.sum = lpbst.sum + rpbst.sum + root.val;
+            spbst.min = Math.min(lpbst.min, Math.min(rpbst.min, root.val));  //left or khud ko
+            spbst.max = Math.max(lpbst.max, Math.max(rpbst.max, root.val));  //left or khud ko
+            if (lpbst.isbst && rpbst.isbst && (lpbst.max < root.val && rpbst.min > root.val)) {
+                spbst.isbst = true;
+                spbst.ans = Math.max(spbst.sum, Math.max(lpbst.ans, rpbst.ans));
+                return spbst;
+            }
+            spbst.isbst = false;
+            spbst.ans = Math.max(lpbst.ans, rpbst.ans);
+            return spbst;
+
+
+        }
+    }
+}
